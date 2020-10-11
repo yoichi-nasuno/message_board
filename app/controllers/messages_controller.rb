378787c1fb658c54f3da9accc331338d6cad6842
yoich-nasuno <nyoichi223@gmail.com>
2020-10-11 05:18:27 +0000

    パーシャル完了

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,10 +1,12 @@
 class MessagesController < ApplicationController
+  
+  before_action :set_message,only: [:show, :edit, :update, :destroy]
   def index
     @messages = Message.all
   end
   
   def show
-    @message = Message.find(params[:id])
+    # @message = Message.find(params[:id])
   end
 
   def new
@@ -24,11 +26,11 @@ class MessagesController < ApplicationController
   end
 
   def edit
-    @message = Message.find(params[:id])
+    # @message = Message.find(params[:id])
   end 
   
   def update
-    @message = Message.find(params[:id])
+    # @message = Message.find(params[:id])
     
     if @message.update(message_params)
       flash[:success] = 'Message は正常に更新されました'
@@ -40,7 +42,7 @@ class MessagesController < ApplicationController
   end 
   
   def destroy
-    @message = Message.find(params[:id])
+    # @message = Message.find(params[:id])
     @message.destroy
     
     flash[:success] = 'Message は正常に削除されました'
@@ -48,6 +50,10 @@ class MessagesController < ApplicationController
   end 
   
   private
+  
+  def set_message
+    @message = Message.find(params[:id])
+  end 
   
   # Strong Parameter
   def message_params
